d4ff440c9d75f9ad70ca06c5e83234eb6d5e8330
tmothy khalayi <nandwatimothykhalayi@GMAIL.COM>
2025-05-16 13:57:10 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,4 @@
-<<<<<<< HEAD
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
-=======
 import './style.css';
 import { CartDB } from './cartdb';
 import type { Dessert } from './cartdb';
@@ -131,4 +106,4 @@ async function init() {
 }
 
 init();
->>>>>>> 1e8fd4f (update)
+
